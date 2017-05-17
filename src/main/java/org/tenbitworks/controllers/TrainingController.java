9309012fd3beeffdb78451cec4b996ece2161849
Mikel Duke <mikelduke@mdp3.net>
2017-05-16 22:10:02 -0500

    remove uneeded iterations

--- src/main/java/org/tenbitworks/controllers/TrainingController.java
+++ src/main/java/org/tenbitworks/controllers/TrainingController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.tenbitworks.model.Asset;
 import org.tenbitworks.model.Training;
 import org.tenbitworks.repositories.AssetRepository;
 import org.tenbitworks.repositories.TrainingRepository;
@@ -26,24 +25,8 @@ public class TrainingController {
     @RequestMapping(value="/trainings/{id}", method = RequestMethod.GET)
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     public String getTraining(@PathVariable Long id, Model model){
-    	Iterable<Training> trainingIterable = trainingRepository.findAll();
-        Iterable<Asset> assetIterable = assetRepository.findAll();
-        
-//        for(Training training : trainingIterable){
-//            long assetId = training.getAssetId();
-//
-//            for(Asset asset : assetIterable){
-//                long id = asset.getId();
-//                String assetTitle = asset.getTitle();
-//                if(assetId == id){
-//                    training.setAssetTitle(assetTitle);
-//                    break;
-//                }
-//            }
-//        }
-
-        model.addAttribute("trainings",trainingIterable );
-        model.addAttribute("assets",assetIterable);
+        model.addAttribute("trainings", trainingRepository.findAll());
+        model.addAttribute("assets", assetRepository.findAll());
         model.addAttribute("training", trainingRepository.findOne(id));
         return "trainings";
     }
@@ -59,24 +42,8 @@ public class TrainingController {
     @RequestMapping(value = "/trainings", method = RequestMethod.GET)
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     public String trainingsList(Model model){
-        Iterable<Training> trainingIterable = trainingRepository.findAll();
-        Iterable<Asset> assetIterable = assetRepository.findAll();
-
-        for(Training training : trainingIterable){
-            long assetId = training.getAssetId();
-
-            for(Asset asset : assetIterable){
-                long id = asset.getId();
-                String assetTitle = asset.getTitle();
-                if(assetId == id){
-                    training.setAssetTitle(assetTitle);
-                    break;
-                }
-            }
-        }
-
-        model.addAttribute("trainings",trainingIterable );
-        model.addAttribute("assets",assetIterable);
+        model.addAttribute("trainings", trainingRepository.findAll());
+        model.addAttribute("assets", assetRepository.findAll());
         
         return "trainings";
     }
@@ -96,5 +63,4 @@ public class TrainingController {
         trainingRepository.delete(id);
         return id.toString();
     }
-
 }
