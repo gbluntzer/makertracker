fdda650c3e670a32a510108f5a10545810dd02e8
Mikel Duke <mikelduke@mdp3.net>
2017-05-24 20:01:33 -0500

    require valid member uuid

--- src/main/java/org/tenbitworks/model/NewUser.java
+++ src/main/java/org/tenbitworks/model/NewUser.java
@@ -2,6 +2,7 @@ package org.tenbitworks.model;
 
 import java.util.List;
 
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
@@ -14,8 +15,9 @@ public class NewUser {
 	@NotBlank
 	private String plainPassword;
 	
-	@NotBlank
-	private String memberName;
+	@NotNull
+	@OneToOne
+	private Member member;
 	
 	@NotNull
 	private List<String> roles;
@@ -32,11 +34,11 @@ public class NewUser {
 	public void setPlainPassword(String plainPassword) {
 		this.plainPassword = plainPassword;
 	}
-	public String getMemberName() {
-		return memberName;
+	public Member getMember() {
+		return member;
 	}
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
+	public void setMember(Member member) {
+		this.member = member;
 	}
 	public List<String> getRoles() {
 		return roles;
@@ -46,47 +48,6 @@ public class NewUser {
 	}
 	@Override
 	public String toString() {
-		return "NewUser [username=" + username + ", memberName=" + memberName + ", roles=" + roles + "]";
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((memberName == null) ? 0 : memberName.hashCode());
-		result = prime * result + ((plainPassword == null) ? 0 : plainPassword.hashCode());
-		result = prime * result + ((roles == null) ? 0 : roles.hashCode());
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		NewUser other = (NewUser) obj;
-		if (memberName == null) {
-			if (other.memberName != null)
-				return false;
-		} else if (!memberName.equals(other.memberName))
-			return false;
-		if (plainPassword == null) {
-			if (other.plainPassword != null)
-				return false;
-		} else if (!plainPassword.equals(other.plainPassword))
-			return false;
-		if (roles == null) {
-			if (other.roles != null)
-				return false;
-		} else if (!roles.equals(other.roles))
-			return false;
-		if (username == null) {
-			if (other.username != null)
-				return false;
-		} else if (!username.equals(other.username))
-			return false;
-		return true;
+		return "NewUser [username=" + username + ", member=" + member + ", roles=" + roles + "]";
 	}
 }
