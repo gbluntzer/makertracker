09480ecd7bdaa9b52c5cfe7b231321fb38c19fef
mikel <mikelduke@mdp3.net>
2017-06-05 20:06:27 -0500

    move js functions to util.js

--- src/main/resources/static/script/member.js
+++ src/main/resources/static/script/member.js
@@ -78,18 +78,3 @@ $(document).ready(function () {
        window.history.pushState('Edit Member', 'MakerTracker', '/members');
    });
 });
-
-function formToObject(form) {//serialize data function
-	var formArray = form.serializeArray();
-	var returnArray = {};
-	for (var i = 0; i < formArray.length; i++){
-		returnArray[formArray[i]['name']] = formArray[i]['value'];
-	}
-	return returnArray;
-}
-
-function resetForm($form) {
-    $form.find('input:text, input:password, input:file, select, textarea').val('');
-    $form.find('input:radio, input:checkbox')
-         .removeAttr('checked').removeAttr('selected');
-}
---
+++ src/main/resources/static/script/util.js
@@ -0,0 +1,14 @@
+function formToObject(form) {//serialize data function
+	var formArray = form.serializeArray();
+	var returnArray = {};
+	for (var i = 0; i < formArray.length; i++){
+		returnArray[formArray[i]['name']] = formArray[i]['value'];
+	}
+	return returnArray;
+}
+
+function resetForm($form) {
+    $form.find('input:text, input:password, input:file, select, textarea').val('');
+    $form.find('input:radio, input:checkbox')
+         .removeAttr('checked').removeAttr('selected');
+}
