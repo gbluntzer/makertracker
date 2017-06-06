9430af2746d50f6ec3fed3abdc5feff11fa783dc
mikel <mikelduke@mdp3.net>
2017-06-05 19:46:40 -0500

    change to use serialize for member edit form

--- src/main/resources/static/script/member.js
+++ src/main/resources/static/script/member.js
@@ -1,72 +1,56 @@
 $(document).ready(function () {
 
-   $('#btn_submit').on("click",function (e) {
-       e.preventDefault();
-       var memberId, memberName, memberEmail, phoneNumber, paymentMethod, description, zipCode, csrf;
-       memberId = $('#member_id').val();
-       memberName = $('#memberName').val();
-       memberEmail = $('#member_email').val();
-       status = $('#status').val();
-       phoneNumber = $('#phoneNumber').val();
-       description = $('#description').val();
-       paymentMethod = $('#paymentMethod').val();
-       zipCode = $('#zipCode').val();
-       csrf = $("[name='_csrf']").val();
-       if($.trim(memberName) === ""){
-           alert("Membername cannot be empty");
-       } else if($.trim(memberEmail) === ""){
-           alert("Member email cannot be empty");
-       } else if ($.trim(status) === "Select One") {
-    	   alert("Select a status");
-       } else {
-           var data = {};
-           if(memberId){
-            data["id"] = memberId;
-           }
-           data["memberName"] = memberName;
-           data["email"] = memberEmail;
-           data["status"] = status;
-           data["phoneNumber"] = phoneNumber;
-           data["paymentMethod"] = paymentMethod;
-           data["description"] = description;
-           data["zipCode"] = zipCode;
+	$('#btn_submit').on("click",function (e) {
+		e.preventDefault();
 
-           $.ajax({
-               headers: { 'X-CSRF-TOKEN': csrf},
-               type: "POST",
-               contentType: "application/json",
-               url: "/members",
-               data: JSON.stringify(data),
-               dataType: 'json',
-               timeout: 6000,
-               success: function (data) {
-                   if (confirm("Member Saved")) {
-                       window.location.reload();
-                   }
-               }
-           });
-       }
-   });
+		var formAr = formArrayToObject($('form').serializeArray());
+		var memberId = $('#member_id').val();
+		var csrf = $("[name='_csrf']").val();
 
-    //Complete order button handler
-   $('.delete-member').on("click", function(e){
-       e.preventDefault();
-       if(confirm("Delete?")){
-           var Id = $(this).closest("td").attr("id");
-           var csrf = $("[name='_csrf']").val();
-           $.ajax({
-                headers: { 'X-CSRF-TOKEN': csrf},
-               type:"DELETE",
-               url:"/members/" + Id,
-               success:function (data) {
-                   $(".delete-order").closest("td#"+data).parent("tr").fadeOut("slow",function(){
-                       $(".delete-order").closest("td#"+data).parent("tr").remove();
-                   });
-                   window.location.reload();
-               }
-           });
-       }
-   });
+		if($.trim(formAr['memberName']) === ""){
+			alert("Membername cannot be empty");
+		} else if($.trim(formAr.email) === ""){
+			alert("Member email cannot be empty");
+		} else if ($.trim(formAr.status) === "Select One") {
+			alert("Select a status");
+		} else {
+			if(memberId){
+				formAr["id"] = memberId;
+			}
+
+			$.ajax({
+				headers: { 'X-CSRF-TOKEN': csrf},
+				type: "POST",
+				contentType: "application/json",
+				url: "/members",
+				data: JSON.stringify(formAr),
+				dataType: 'json',
+				timeout: 6000,
+				success: function (data) {
+					if (confirm("Member Saved")) {
+						window.location.reload();
+					}
+				}
+			});
+		}
+	});
+
+	//Complete order button handler
+	$('.delete-member').on("click", function(e){
+		e.preventDefault();
+		if(confirm("Delete?")){
+			var Id = $(this).closest("td").attr("id");
+			var csrf = $("[name='_csrf']").val();
+			$.ajax({
+				headers: { 'X-CSRF-TOKEN': csrf},
+				type:"DELETE",
+				url:"/members/" + Id,
+				success:function (data) {
+					window.location.reload();
+				}
+			});
+		}
+	});
 
    $('.edit-member').on("click", function(e){
        e.preventDefault();
@@ -94,17 +78,21 @@ $(document).ready(function () {
    
    $('.new-member').on("click", function(e){
        e.preventDefault();
-
-       $('#member_id').val('');
-       $('#memberName').val('');
-       $('#status').get(0).selectedIndex = 0;
-       $('#member_email').val('');
-       $('#phoneNumber').val('');
-       $('#paymentMethod').get(0).selectedIndex = 0;
-       $('#description').val('');
-       $('#zipCode').val('');
-       $('#username').val('');
-       
+       resetForm($('#form'));
        window.history.pushState('Edit Member', 'MakerTracker', '/members');
    });
 });
+
+function formArrayToObject(formArray) {//serialize data function
+	var returnArray = {};
+	for (var i = 0; i < formArray.length; i++){
+		returnArray[formArray[i]['name']] = formArray[i]['value'];
+	}
+	return returnArray;
+}
+
+function resetForm($form) {
+    $form.find('input:text, input:password, input:file, select, textarea').val('');
+    $form.find('input:radio, input:checkbox')
+         .removeAttr('checked').removeAttr('selected');
+}
