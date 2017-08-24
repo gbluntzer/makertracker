78fbacb2229e8378820aec0ca7e7f7d8d0f55e38
mikel <mikelduke@mdp3.net>
2017-08-23 22:29:59 -0500

    delete news item

--- src/main/resources/static/script/news.js
+++ src/main/resources/static/script/news.js
@@ -7,6 +7,25 @@ $(document).ready(function () {
 
 		$('#editNewsForm').toggle();
 	});
+	
+	$('.delete-news-item').on("click", function(e) {
+		e.preventDefault();
+		$('#editNewsForm').hide();
+		
+		if(confirm("Delete?")){
+			var id = $(this).closest(".newsitem").attr("id");
+			var csrf = $("[name='_csrf']").val();
+
+			$.ajax({
+				headers: { 'X-CSRF-TOKEN': csrf},
+				type:"DELETE",
+				url:"/news/" + id,
+				success:function (data) {
+					window.location.reload();
+				}
+			});
+		}
+	});
 
 	$('#btn_editNews').on("click", function(e){
 		e.preventDefault();
